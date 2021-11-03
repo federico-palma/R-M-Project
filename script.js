4d9be27d73cdc45fb7f41e3799f2259a9ed3467b
Federico Palma <fedepal9@gmail.com>
2021-11-03 11:31:09 +0100

    Detail cards added on click

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const baseApiUrl = 'https://rickandmortyapi.com/api/character';
 let apiData;
+let charApiData;
 let cardTable = document.getElementById('card-table');
 
-// Function fetches API, returns all characters data in json format.
+// Function fetches API, returns data in json format.
 async function fetchApi(url) {
     let response = await fetch(url);
     let data = await response.json();
@@ -40,14 +41,41 @@ async function setCharContent(apiUrl) {
             cardElements[2].innerText = apiData.results[i].name;
             cardElements[3].innerText = apiData.results[i].id;
             cardTable.appendChild(cardElements[0]);
+
+            // Set Event listener for detail card pop up.
+            cardElements[0].addEventListener('click', () => {
+                showDetailsCard(cardElements[3].innerText);
+            });
         }
     } else {
-        console.log('All characters loaded.')
+        console.log('API Data not loaded');
     }
 }
 
 // Set initial cards.
 setCharContent(baseApiUrl);
+
+// Details card code.
+let detailsCard = document.querySelector('.card-detail');
+
+async function showDetailsCard(charID) {
+    charApiData = await fetchApi(baseApiUrl + '/' + charID)
+    console.log(charApiData);
+    detailsCard.style.visibility = 'visible';
+
+    document.getElementById('char-id-detail').innerText = charID;
+    document.getElementById('char-img-detail').src = charApiData.image;
+    document.getElementById("char-name-detail").innerText = charApiData.name;
+    document.getElementById("char-gender-detail").innerText = charApiData.gender;
+    document.getElementById("char-origin-detail").innerText = charApiData.origin.name;
+    document.getElementById("char-status-detail").innerText = charApiData.status;
+}
+
+// Close details card.
+let closeBtn = document.getElementById('close-btn');
+closeBtn.addEventListener('click', () => {
+    detailsCard.style.visibility = 'hidden';
+});
 
 // Event listener for infinte scrolling and loading animation.
 window.addEventListener('scroll', () => {
@@ -56,6 +84,8 @@ window.addEventListener('scroll', () => {
         showLoading();
         setCharContent(apiData.info.next);
         hideLoading();
+    } else if (scrollTop + clientHeight >= scrollHeight && apiData.info.next == null){
+        console.log('All characters loaded.')
     }
 });
 
@@ -68,6 +98,7 @@ function showLoading() {
 function hideLoading() {
     loadingAnimElements.classList.remove('show')
 };
+
 
 
 // Function to resize navbar 
